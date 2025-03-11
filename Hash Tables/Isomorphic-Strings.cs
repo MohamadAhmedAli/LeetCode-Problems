94d3e6a2c4d88a61506c01e3af10fc8419c67705
Mohamed Ahmed <112741669+MohamadAhmedAli@users.noreply.github.com>
2025-03-11 22:13:15 +0200

    [205] [Time Beats: 43.84%] [Memory Beats: 96.90%] - LeetPush

---
+++ Hash Tables/Isomorphic-Strings.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+    int num = 0,num2=0;
+    Dictionary<char, char> dict = new Dictionary<char, char>();
+    Dictionary<char, char> dict2 = new Dictionary<char, char>();
+    foreach (char c in s)
+    {
+        if()
+    }
+    
+
+    return true;
+}
+}
